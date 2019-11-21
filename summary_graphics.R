aea9f6aaf4c7a567995caa36f9f691c1c5150bb6
Daniella Lato <latodf@mcmaster.ca>
2019-11-20 20:30:10 -0500

    better attmept at graph
    need to fix legend, maybe add omega =1 label

--- summary_graphics.R
+++ summary_graphics.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(data.table)
 
+options(scipen=10000)
 theme_set(theme_bw() + theme(strip.background =element_rect(fill="#e7e5e2")) +
             theme(strip.text = element_text(size =10)) +
             #theme(plot.title = element_text(hjust = 0.5), 
@@ -11,6 +12,7 @@ theme_set(theme_bw() + theme(strip.background =element_rect(fill="#e7e5e2")) +
                   panel.grid.minor = element_line(colour = "grey98", size = 0.5), 
                   panel.spacing = unit(0.25, "lines"), 
                   axis.text=element_text(size=10),
+                  legend.title = element_blank(),
                   legend.position="top") 
 )
 
@@ -64,28 +66,33 @@ num_of_plots <- length(levels(selection_df_no_NA$bacteria))
 #italic bacteria names
   levels(selection_df_no_NA$bacteria) <- c("ecoli" = expression(paste(italic("E.coli"), "")),
                           "bass" = expression(paste(italic("B. subtilis"), "")),
-                          "sinoC" = expression(paste(italic("S.meliloti"), "")),
+                          "sinoC" = expression(paste(italic("S.meliloti"), " Chromosome")),
                           "pSymA" = expression(paste(italic("S.meliloti"), " pSymA")),
-                          "strep" = expression(paste(italic("Streptomyces"), " Chromosome`")),
+                          "strep" = expression(paste(italic("Streptomyces"), "")),
                           "pSymB" = expression(paste(italic("S.meliloti"), " pSymB")))
 
 #choose colours
 colours_arr <- rep(c("#CFE7C8","#D2E4DC","#A0747A"),num_of_plots)
 #plot
-vio_str<-(ggplot(selection_df_no_NA, aes(x=sel_class, y=val, fill=sel_class)) 
-          + geom_violin() 
+set.seed(1738)
+vio_str_box <-(ggplot(selection_df_no_NA, aes(x=sel_class, y=val, fill = sel_class, colour = sel_class)) 
           + geom_jitter(position=position_jitter(0.2))
-          + geom_boxplot(width=.1, outlier.shape=NA, fill = colours_arr) 
-          + stat_boxplot(geom = "errorbar", width = 0.2) 
+          + geom_violin(colour = "black", fill = NA) 
+          #+ geom_boxplot(width=.1, outlier.shape=NA, fill = colours_arr) 
+          + geom_boxplot(width=.1, outlier.shape=NA, colour = "black", fill = colours_arr) 
+          + stat_boxplot(geom = "errorbar", width = 0.2, colour = "black") 
+          #omega = 1 reference line
+          +  geom_hline(yintercept=1, linetype="dashed", color = "#023C40")
           + facet_wrap(~bacteria, labeller=label_parsed)
           + xlab("") 
           + ylab("Value") 
-          + scale_color_manual(values=colours_arr)
-          #make the omega a math symbol in both the legend and x-axis
+          + scale_color_manual(values=c("#CFE7C8","#D2E4DC","#A0747A"),labels = c(" dN", " dS", expression(omega)))
+          #+ scale_color_manual(values=c("#CFE7C8","#D2E4DC","#A0747A"))
+          #make the omega a math symbol in x-axis
           + scale_x_discrete(breaks = c("dN", "dS", "omega"),labels = c("dN","dS", expression(omega))) 
-          #log scale
-          #+ scale_y_continuous(trans='log10')
-          + scale_fill_manual(values=c("#CFE7C8","#D2E4DC","#A0747A"), labels = c(" dN", " dS", expression(omega))) 
+          #log scale and removing trailing zeros from y-axis labels
+          + scale_y_continuous(trans='log10',labels = function(x) ifelse(x == 0, "0", x))
+          #+ scale_fill_manual(values=c("#CFE7C8","#D2E4DC","#A0747A"), labels = c(" dN", " dS", expression(omega))) 
 )
 
-vio_str
+vio_str_box
